641897ef0d7bc463a85d243603d751bc35e5f5f0
Tan-maddy <82451665+Tan-maddy@users.noreply.github.com>
2022-03-03 21:00:50 +0530

    Add files via upload

---
+++ NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void LoadScene(){
+        SceneManager.LoadScene("MainScene")
+    }
+
+    public void ExitApplication(){
+
+        Application.Quit();
+        Debug.Log("Project is closed")
+    }
+
+    public void LoadMenuScene(){
+        SceneManager.LoadScene("MainMenu")
+    }
+    
+}
---
+++ randomdmm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class randomdmm : MonoBehaviour
+{
+// Start is called before the first frame update
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("MainScene");
+
+    }
+
+// Update is called once per frame
+    public void ExitApplication()
+    {
+        Application.Quit();
+        Debug.Log("Project is closed");
+    }
+    public void LoadMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
